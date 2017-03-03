c8320301158c135484c2c14f215d0c19ad6edbab
Jordan Ramey <jramey@qualtrax.com>
2017-03-02 20:14:23 -0500

    Control Angular 2 Events with  and Event Handlers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<div><app-simple-form></app-simple-form>'
+  template: '<div><app-simple-form></app-simple-form><div>'
 })
 export class AppComponent {
   title = 'Lets get started!';
