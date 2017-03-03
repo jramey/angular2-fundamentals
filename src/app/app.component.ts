f66dbdf680735d2408de8dd52263cdb36bdaaba2
Jordan Ramey <jramey@qualtrax.com>
2017-03-02 19:58:56 -0500

    Create a Simple Angular 2 Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<div><app-simple-form></app-simple-form>'
 })
 export class AppComponent {
   title = 'Lets get started!';
 }
+
