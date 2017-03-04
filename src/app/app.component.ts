bad7415e128c6a967f8626a4a93ba2c9d2c16814
Jordan Ramey <jordanramey@Jordans-MacBook-Air.local>
2017-03-03 20:00:48 -0500

    Pass Events from Angular 2 Components with @Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,18 @@ import { Component, Inject } from '@angular/core';
   template: `
 <div>
   <ul>
-    <app-simple-form *ngFor="let message of mail.messages" [message]="message">
-    </app-simple-form>
+    <li *ngFor="let message of mail.messages">{{ message.text }}</li>
   </ul>
+    <app-simple-form *ngFor="let message of mail.messages" 
+    [message]="message.text"
+    (update)="onUpdate(message.id, $event.text)">
+    </app-simple-form>  
 </div>`
 })
 export class AppComponent {
-  title = 'Lets get started!';
+  onUpdate(id, text) {
+    this.mail.update(id, text);
+  }
 
   constructor(@Inject('mail') private mail) {}
 }
--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-simple-form',
   template: `<div>
-    {{ message }}
     <input #myInput type="text" [(ngModel)]="message">
-    <button (mouseover)="onClick($event, myInput.value)">Click me!</button>
+    <button (click)="update.emit({ text:message })">Click me!</button>
   </div>
   `,
   styles: []
@@ -13,15 +12,9 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SimpleFormComponent implements OnInit {
 
   @Input() message;
-  
-  onClick(event, value) {
-    console.log(event);
-    console.log(value);
-  }
+  @Output() update = new EventEmitter();
 
-  constructor() { 
-    setInterval(() => this.message = Math.random().toString(), 1000)
-  }
+  constructor() { }
 
   ngOnInit() { }
 
